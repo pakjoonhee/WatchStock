c88af25aefab83c03c1818c5b821e62fbd169441
Joonhee <pakjoonhee@gmail.com>
2016-06-18 21:37:25 -0700

    Got the chart to work kind of

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/LineGraphActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/LineGraphActivity.java
@@ -6,13 +6,19 @@ import android.content.Context;
 import android.content.OperationApplicationException;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.db.chart.Tools;
+import com.db.chart.model.LineSet;
+import com.db.chart.view.AxisController;
 import com.db.chart.view.ChartView;
+import com.db.chart.view.LineChartView;
+import com.db.chart.view.animation.Animation;
 import com.google.android.gms.gcm.GcmNetworkManager;
 import com.google.android.gms.gcm.GcmTaskService;
 import com.google.android.gms.gcm.TaskParams;
@@ -47,15 +53,19 @@ import java.util.concurrent.ExecutionException;
 public class LineGraphActivity extends Activity {
     private String BASE_URL = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20";
     private String END_URL = "%20and%20startDate%20%3D%20\"2009-09-11\"%20and%20endDate%20%3D%20\"2010-03-10\"&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
-    private ArrayList<String> stockHistory = new ArrayList<>();
-    private ArrayList<String> retrievedStockHistory = new ArrayList<>();
+    private ArrayList<Double> stockHistory = new ArrayList<>();
+    private ArrayList<Double> retrievedStockHistory = new ArrayList<>();
+    private final String[] mLabels = {"o", "p", "q", "r"};
+    private final float[] mValues = {12f, 80f, 60f, 80f};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.simple_line_graph);
+        setContentView(R.layout.activity_line_graph);
         Bundle bundle = getIntent().getExtras();
         String symbol = bundle.getString("symbol");
+
+
 
         //StringBuilder urlStringBuilder = new StringBuilder();
         //try {
@@ -74,16 +84,33 @@ public class LineGraphActivity extends Activity {
             e.printStackTrace();
         }
 
-        ChartView chartView = (ChartView)findViewById(R.id.linechart);
+        LineChartView lineChartView = (LineChartView) findViewById(R.id.linechart);
+
+        LineSet dataset = new LineSet(mLabels, mValues);
+        dataset.setColor(Color.parseColor("#53c1bd"))
+                .setFill(Color.parseColor("#3d6c73"))
+                .setGradientFill(new int[]{Color.parseColor("#364d5a"), Color.parseColor("#3f7178")}, null);
+        lineChartView.addData(dataset);
+
+        lineChartView.setBorderSpacing(10)
+                .setXLabels(AxisController.LabelPosition.NONE)
+                .setYLabels(AxisController.LabelPosition.NONE)
+                .setXAxis(false)
+                .setYAxis(false)
+                .setBorderSpacing(Tools.fromDpToPx(10));
+
+        Animation anim = new Animation();
+        lineChartView.addData(dataset);
+        lineChartView.show();
 
 
     }
-    public class AsyncHttpTask extends AsyncTask<String, Void, ArrayList<String>> {
+    public class AsyncHttpTask extends AsyncTask<String, Void, ArrayList<Double>> {
         HttpURLConnection connection = null;
 
         @Override
-        protected ArrayList<String> doInBackground(String... params) {
-            ArrayList<String> result = null;
+        protected ArrayList<Double> doInBackground(String... params) {
+            ArrayList<Double> result = null;
             try {
                 URL url = new URL(params[0]);
                 connection = (HttpURLConnection) url.openConnection();
@@ -97,7 +124,7 @@ public class LineGraphActivity extends Activity {
             return result;
         }
 
-        private ArrayList<String> parseReview(String result) {
+        private ArrayList<Double> parseReview(String result) {
             JSONArray resultsArray = null;
             JSONObject jsonObject = null;
 
@@ -106,7 +133,7 @@ public class LineGraphActivity extends Activity {
                 resultsArray = jsonObject.getJSONObject("query").getJSONObject("results").getJSONArray("quote");
                 for (int i = 0; i < resultsArray.length(); i++) {
                     jsonObject = resultsArray.optJSONObject(i);
-                    String stockPrice = jsonObject.optString("Close");
+                    Double stockPrice = jsonObject.optDouble("Close");
                     stockHistory.add(stockPrice);
                 }
             } catch (JSONException e) {
@@ -116,7 +143,7 @@ public class LineGraphActivity extends Activity {
         }
 
         @Override
-        protected void onPostExecute(ArrayList<String> result) {
+        protected void onPostExecute(ArrayList<Double> result) {
             super.onPostExecute(result);
 
         }
