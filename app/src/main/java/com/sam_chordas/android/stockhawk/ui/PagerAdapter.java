e1544896f383253f498fb2bbb5ab781f05f78b19
Joonhee <pakjoonhee@gmail.com>
2016-11-25 21:56:58 -0800

    feat: slowly implementing retrofit

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/PagerAdapter.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/PagerAdapter.java
@@ -1,10 +1,8 @@
 package com.sam_chordas.android.stockhawk.ui;
 
-import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentStatePagerAdapter;
 
 import java.util.ArrayList;
 
